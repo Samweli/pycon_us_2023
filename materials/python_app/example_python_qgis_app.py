4e4565c30ca37c191c6af0af68f5e9d1961c03a5
samweli <smwltwesa6@gmail.com>
2023-04-20 19:11:24 +0300

    python app with Qgis

---
+++ materials/python_app/example_python_qgis_app.py
@@ -0,0 +1,34 @@
+import sys
+from qgis.core import *
+from qgis.gui import QgsMapCanvas
+from qgis.PyQt.QtWidgets import QApplication, QFrame, QGridLayout, QMainWindow
+
+application = QApplication(sys.argv)
+
+QgsApplication.setPrefixPath('/usr/bin/qgis', True)
+QgsApplication.initQgis()
+
+main_window = QMainWindow()
+main_window.setWindowTitle(
+    "Python with QGIS example | "
+    "PyCon US 2023"
+)
+frame = QFrame()
+main_window.setCentralWidget(frame)
+layout = QGridLayout(frame)
+
+map_canvas = QgsMapCanvas()
+layout.addWidget(map_canvas)
+
+raster_file_uri = 'https://github.com/qgis/QGIS-Sample-Data/' \
+                  'blob/master/qgis_sample_data/raster/' \
+                  'SR_50M_alaska_nad.tif?raw=true'
+
+raster_layer = QgsRasterLayer(raster_file_uri, 'test_layer')
+print(raster_layer.isValid())
+QgsProject.instance().addMapLayer(raster_layer)
+map_canvas.setLayers([raster_layer])
+map_canvas.setExtent(raster_layer.extent())
+
+main_window.show()
+application.exec_()
